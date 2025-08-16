fd7819d5fda1f1d5c90918bdeb6620e5dcf6dac1
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2025-08-16 15:13:32 +0300

    adding random library

--- randomNumber.py
+++ randomNumber.py
@@ -1,3 +1,21 @@
+import random
+
 print("welcome to random number generator!")
-print("Your job is to find the generated number in less than 4 tries!")
-print("Do you want to play?(Y/N) : ")
+print("Your job is to find the generated number in less than 5 tries!")
+input("Do you want to play?(Y/N) : ").lower()
+
+def play():
+    guessed = False
+    guesses = 0
+    randomNumber = random.randint(1,10)
+    while guesses < 5:
+        yourGuess = int(input("Your guess : "))
+        if(yourGuess == randomNumber):
+            guessed = True
+            print("Congrats, you guesses the number!")
+            break
+    if guessed == False:
+        print("You lost!")
+
+def quit():
+    print("Maybe later!")
