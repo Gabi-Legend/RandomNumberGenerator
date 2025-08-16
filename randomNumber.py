988100149c3b4795b815df1425d9fdc50be56265
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2025-08-16 15:19:46 +0300

    bug fixes

--- randomNumber.py
+++ randomNumber.py
@@ -1,30 +1,38 @@
 import random
 
-print("welcome to random number generator!")
+print("Welcome to random number generator!")
 print("Your job is to find the generated number in less than 5 tries!")
-play = input("Do you want to play?(Y/N) : ").lower()
+play = input("Do you want to play? (Y/N): ").lower()
 
 def playGame():
     guessed = False
     guesses = 0
-    randomNumber = random.randint(1,10)
+    randomNumber = random.randint(1, 10)
+    
     while guesses < 5:
-        yourGuess = int(input("Your guess : "))
-        if(yourGuess == randomNumber):
-            guessed = True
-            guesses+=1
-            print(f"Congrats, you guesses the number in {guesses} tries!")
-            break
-        else:
-            print("Try again")
-            guesses+=1
-    if guessed == False:
-        print("You lost!")
+        try:
+            yourGuess = int(input("Your guess: "))
+        except ValueError:
+            print("Please enter a valid number!")
+            continue
 
-def quit():
+        guesses += 1
+        if yourGuess == randomNumber:
+            guessed = True
+            print(f"Congrats, you guessed the number in {guesses} tries!")
+            break
+        elif yourGuess < randomNumber:
+            print("Too low, try again.")
+        else:
+            print("Too high, try again.")
+
+    if not guessed:
+        print(f"You lost! The number was {randomNumber}.")
+
+def exitGame():
     print("Maybe later!")
 
-if play == "yes" or play == "da" or play == "y":
+if play in ["yes", "da", "y"]:
     playGame()
 else:
-    quit()
+    exitGame()
